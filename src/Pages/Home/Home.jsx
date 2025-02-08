4bfe6f9714951977f44bf4d5117af4fc73a101af
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-02-08 13:12:12 +0100

    updated both navbar and home components

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,31 @@
 import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import hero_image from '../../assets/hero_banner.jpg'
+import hero_title from '../../assets/hero_title.png'
+import play_image from '../../assets/play_icon.png'
+import info_image from '../../assets/info_icon.png'
 
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Home page!!!!</h1>
+            <Navbar/>
+            <div className='hero'>
+                <img src={hero_image} alt='hero-banner'/>
+                <div className='hero-caption'>
+                    <img src={hero_title} alt='banner-img'/>
+                    <p>
+                        Discovering his ties to a secret ancient order, a youn man living in modern Istanbul embarks on a quest to save city from an immortal enemy.
+                    </p>
+                    <div className='hero-btns'>
+                        <button className='btn'>
+                            <img src={play_image} alt="play-button"/>Play
+                        </button>
+                        <button className='btn dark-btn'>
+                            <img src={info_image} alt="more-info-button"/>More Info
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,63 @@
-import './Navbar.css'
+import { useState, useRef, useEffect } from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+import search_logo from '../../assets/search_icon.svg';
+import bell_logo from '../../assets/bell_icon.svg';
+import profile_logo from '../../assets/profile_img.png';
+import caret_logo from '../../assets/caret_icon.svg';
 
 const Navbar = () => {
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef(null);
+
+    const dropdownHandler = () => {
+        setShowDropdown(prev => !prev);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowDropdown(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
     return (
         <div className='navbar'>
-            <h1>Navbarr</h1>
+            <div className='navbar-left'>
+                <div>
+                    <img src={logo} alt='company-logo'/>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>TV Shows</li>
+                    <li>Movies</li>
+                    <li>New & Popular</li>
+                    <li>My List</li>
+                    <li>Browse by Language</li>
+                </ul>
+            </div>
+            <div className='navbar-right'>
+                <img src={search_logo} alt='search-logo' className='icons'/>
+                <p>Children</p>
+                <img src={bell_logo} alt='notification-logo' className='icons'/>
+                <div className='navbar-profile' onClick={dropdownHandler} ref={dropdownRef}>
+                    <img src={profile_logo} alt='profile-logo' className='profile'/>
+                    <img src={caret_logo} alt='caret-logo' className='icons'/>
+                    {showDropdown && (
+                        <div className='profile-dropdown'>
+                            <p>Sign out of Netflix</p>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
