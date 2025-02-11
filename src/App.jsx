0edd6dee4c3a02c458ca6c5cef5c100144ba2cd4
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-02-11 04:44:21 +0100

    installed react-router and made use of browser-route, routes and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home/Home"
+import Login from "./Pages/Login/Login"
 
 const App = () => {
   return (
     <div>
-      <Home/>
-      
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/login" element={<Login />}/>
+      </Routes>
     </div>
   )
 }
